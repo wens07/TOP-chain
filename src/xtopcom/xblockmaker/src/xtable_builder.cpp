0e274109527fc3f36904306d895d4b55a328972f
LonDong <lon.dong@topnetwork.org>
2021-09-06 16:15:57 +0800

    add workload UT

--- src/xtopcom/xblockmaker/src/xtable_builder.cpp
+++ src/xtopcom/xblockmaker/src/xtable_builder.cpp
@@ -165,6 +165,9 @@ xblock_ptr_t        xfulltable_builder_t::build_block(const xblock_ptr_t & prev_
 
     int64_t tgas_balance_change_total = 0;
     for(auto & block : blocks) {
+        if (block->is_fullblock()) {
+            continue;
+        }
         auto out_entity = block->get_output()->get_primary_entity();
         if (out_entity != nullptr) {
             int64_t tgas_balance_change = base::xstring_utl::toint64(out_entity->query_value(base::xvoutentity_t::key_name_tgas_pledge_change()));
