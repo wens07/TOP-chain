5fe5cb6526fb0e9c1e93bbb0021a428d7fdc8b7d
LonDong <lon.dong@topnetwork.org>
2021-09-13 15:29:10 +0800

    run only rec standby contract

--- src/xtopcom/xblockmaker/src/xunit_builder.cpp
+++ src/xtopcom/xblockmaker/src/xunit_builder.cpp
@@ -2,23 +2,23 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <string>
-#include <cinttypes>
-#include "xvledger/xreceiptid.h"
 #include "xblockmaker/xunit_builder.h"
+
 #include "xblockmaker/xblockmaker_error.h"
+#include "xcontract_vm/xaccount_vm.h"
+#include "xdata/xblockbuild.h"
+#include "xdata/xblocktool.h"
 #include "xdata/xemptyblock.h"
 #include "xdata/xfullunit.h"
-#include "xdata/xblocktool.h"
 #include "xstore/xaccount_context.h"
+#include "xsystem_contract_runtime/xsystem_contract_manager.h"
 #include "xtxexecutor/xtransaction_executor.h"
+#include "xvledger/xreceiptid.h"
 #include "xvledger/xvledger.h"
 #include "xvledger/xvstatestore.h"
-// #include "xcontract_runtime/xaccount_vm.h"
-#include "xdata/xblockbuild.h"
 
-// #include "xcontract_runtime/xsystem_contract_manager.h"
-// #include "xcontract_runtime/xaccount_vm.h"
+#include <cinttypes>
+#include <string>
 
 NS_BEG2(top, blockmaker)
 
@@ -52,46 +52,93 @@ xblock_ptr_t        xlightunit_builder_t::build_block(const xblock_ptr_t & prev_
     xassert(lightunit_build_para != nullptr);
 
     const std::vector<xcons_transaction_ptr_t> & input_txs = lightunit_build_para->get_origin_txs();
-#if 0
-    {
-        xassert(!cs_para.get_table_account().empty());
-        xassert(cs_para.get_table_proposal_height() > 0);
-        xassert(!cs_para.get_random_seed().empty());
 
+    if (input_txs.size() == 1 && 
+        input_txs[0]->get_tx_subtype() == base::enum_transaction_subtype_recv &&
+        input_txs[0]->get_transaction()->get_target_addr() == sys_contract_rec_standby_pool_addr) {
+        xassert(!cs_para.get_table_account().empty());
+        xassert(!cs_para.get_random_seed().empty());
+        xassert(cs_para.get_table_proposal_height() > 0);
+
+        auto system_contract_manager = make_unique<contract_runtime::system::xsystem_contract_manager_t>();
+        system_contract_manager.reset(&contract_runtime::system::xsystem_contract_manager_t::instance());
+        xassert(system_contract_manager != nullptr);
         auto _temp_header = base::xvblockbuild_t::build_proposal_header(prev_block.get());
         auto proposal_bstate = make_object_ptr<base::xvbstate_t>(*_temp_header.get(), *prev_bstate.get());
 
-        auto temp_manager = make_unique<contract_runtime::xsystem_contract_manager_t>();
-        contract_runtime::xaccount_vm_t vm(temp_manager);
+        contract_vm::xaccount_vm_t vm(system_contract_manager);
         auto result = vm.execute(input_txs, proposal_bstate, cs_para);
-    }
-#endif
+        
+        size_t last_success_index = 0;
+        for (auto i = 0u; i < result.transaction_results.size(); i++) {
+            auto const & r = result.transaction_results[i];
+            if (r.status.ec) {
+                input_txs[i]->set_current_exec_status(enum_xunit_tx_exec_status_fail);
+                lightunit_build_para->set_fail_tx(input_txs[i]);
+            } else {
+                input_txs[i]->set_current_exec_status(enum_xunit_tx_exec_status_success);
+                lightunit_build_para->set_pack_tx(input_txs[i]);
+                // for (auto tx : r.output.followup_transaction_data) {
+                //     lightunit_build_para->set_pack_tx(tx.followed_transaction);
+                //     printf("tx %d add one new create tx\n", i);
+                //     xdbg("tx %d fail\n", i);
+                // }
+                last_success_index = i;
+            }
+        }
+        if (result.status.ec) {
+            build_para->set_error_code(xblockmaker_error_tx_execute);
+            return nullptr;
+        }
 
-    txexecutor::xbatch_txs_result_t exec_result;
-    int exec_ret = txexecutor::xtransaction_executor::exec_batch_txs(prev_block.get(), prev_bstate, cs_para, input_txs, exec_result);
-    xinfo("xlightunit_builder_t::build_block %s,account=%s,height=%ld,exec_ret=%d,succtxs_count=%zu,failtxs_count=%zu,unconfirm_count=%d,binlog_size=%zu,binlog=%ld,state_size=%zu",
-        cs_para.dump().c_str(), prev_block->get_account().c_str(), prev_block->get_height() + 1,
-        exec_ret, exec_result.m_exec_succ_txs.size(), exec_result.m_exec_fail_txs.size(),
-        exec_result.m_unconfirm_tx_num, exec_result.m_property_binlog.size(), base::xhash64_t::digest(exec_result.m_property_binlog), exec_result.m_full_state.size());
-    // some send txs may execute fail but some recv/confirm txs may execute successfully
-    if (!exec_result.m_exec_fail_txs.empty()) {
-        lightunit_build_para->set_fail_txs(exec_result.m_exec_fail_txs);
-    }
-    if (exec_ret != xsuccess) {
-        build_para->set_error_code(xblockmaker_error_tx_execute);
-        return nullptr;
-    }
+        // lightunit_build_para->set_pack_txs(input_txs);
+        xlightunit_block_para_t lightunit_para;
+        lightunit_para.set_input_txs(lightunit_build_para->get_pack_txs());
+        lightunit_para.set_fullstate_bin(result.transaction_results[last_success_index].output.contract_state_snapshot);
+        lightunit_para.set_binlog(result.transaction_results[last_success_index].output.binlog);
 
-    lightunit_build_para->set_tgas_balance_change(exec_result.m_tgas_balance_change);
-    lightunit_build_para->set_pack_txs(exec_result.m_exec_succ_txs);
+        base::xreceiptid_state_ptr_t receiptid_state = lightunit_build_para->get_receiptid_state();
+        alloc_tx_receiptid(lightunit_build_para->get_pack_txs(), receiptid_state);
+        base::xvblock_t* _proposal_block = data::xblocktool_t::create_next_lightunit(lightunit_para, prev_block.get(), cs_para);
+        xblock_ptr_t proposal_unit;
+        proposal_unit.attach((data::xblock_t *)_proposal_block);
+        return proposal_unit;
+    } else {
+        txexecutor::xbatch_txs_result_t exec_result;
+        int exec_ret = txexecutor::xtransaction_executor::exec_batch_txs(prev_block.get(), prev_bstate, cs_para, input_txs, exec_result);
+        xinfo(
+            "xlightunit_builder_t::build_block "
+            "%s,account=%s,height=%ld,exec_ret=%d,succtxs_count=%zu,failtxs_count=%zu,unconfirm_count=%d,binlog_size=%zu,binlog=%ld,state_size=%zu",
+            cs_para.dump().c_str(),
+            prev_block->get_account().c_str(),
+            prev_block->get_height() + 1,
+            exec_ret,
+            exec_result.m_exec_succ_txs.size(),
+            exec_result.m_exec_fail_txs.size(),
+            exec_result.m_unconfirm_tx_num,
+            exec_result.m_property_binlog.size(),
+            base::xhash64_t::digest(exec_result.m_property_binlog),
+            exec_result.m_full_state.size());
+        // some send txs may execute fail but some recv/confirm txs may execute successfully
+        if (!exec_result.m_exec_fail_txs.empty()) {
+            lightunit_build_para->set_fail_txs(exec_result.m_exec_fail_txs);
+        }
+        if (exec_ret != xsuccess) {
+            build_para->set_error_code(xblockmaker_error_tx_execute);
+            return nullptr;
+        }
 
-    xlightunit_block_para_t lightunit_para;
-    // set lightunit para by tx result
-    lightunit_para.set_input_txs(exec_result.m_exec_succ_txs);
-    lightunit_para.set_account_unconfirm_sendtx_num(exec_result.m_unconfirm_tx_num);
-    lightunit_para.set_fullstate_bin(exec_result.m_full_state);
-    lightunit_para.set_binlog(exec_result.m_property_binlog);
-    return create_block(prev_block, cs_para, lightunit_para, lightunit_build_para->get_receiptid_state());
+        lightunit_build_para->set_tgas_balance_change(exec_result.m_tgas_balance_change);
+        lightunit_build_para->set_pack_txs(exec_result.m_exec_succ_txs);
+
+        xlightunit_block_para_t lightunit_para;
+        // set lightunit para by tx result
+        lightunit_para.set_input_txs(exec_result.m_exec_succ_txs);
+        lightunit_para.set_account_unconfirm_sendtx_num(exec_result.m_unconfirm_tx_num);
+        lightunit_para.set_fullstate_bin(exec_result.m_full_state);
+        lightunit_para.set_binlog(exec_result.m_property_binlog);
+        return create_block(prev_block, cs_para, lightunit_para, lightunit_build_para->get_receiptid_state());
+    }
 }
 
 std::string     xfullunit_builder_t::make_binlog(const xblock_ptr_t & prev_block,
